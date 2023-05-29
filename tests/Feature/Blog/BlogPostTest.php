3250bdb39f76773ede2ff1723348d1d64e68b434
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2023-05-29 20:29:53 +0300

    add test for time

--- tests/Feature/Blog/BlogPostTest.php
+++ tests/Feature/Blog/BlogPostTest.php
@@ -4,6 +4,8 @@ namespace Tests\Feature\Blog;
 
 use App\Models\BlogPost;
 use App\Models\BlogPostLike;
+use App\Models\Enums\BlogPostStatus;
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -40,5 +42,28 @@ class BlogPostTest extends TestCase
 
         $this->assertCount(5, $post->postLikes);
         $this->assertTrue($postLike->blogPost->isPublished());
+    }
+
+    /**
+     * @test
+     * @return void
+     */
+    public function it_test_published_scope()
+    {
+        BlogPost::factory()->create([
+           'date' => '2021-06-01',
+           'status' => BlogPostStatus::PUBLISHED()
+        ]);
+
+        $this->travelTo(Carbon::make('2021-01-01'));
+
+        $this->assertEquals(0, BlogPost::query()->wherePublished()->count());
+
+        $this->travelTo(Carbon::make('2021-07-01'));
+
+        $this->assertEquals(1, BlogPost::query()->wherePublished()->count());
+
+        // travelBack - to original time
+        // travel(1)->years() - move time forward 1 year
     }
 }
