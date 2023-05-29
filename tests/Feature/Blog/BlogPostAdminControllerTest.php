1c317fad5f5d23236106c00580e927eb3697c056
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2023-05-29 16:52:14 +0300

    add test auth

---
+++ tests/Feature/Blog/BlogPostAdminControllerTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\Feature\Blog;
+
+use App\Http\Controllers\BlogPostAdminController;
+use App\Models\BlogPost;
+use App\Models\User;
+use Tests\TestCase;
+
+class BlogPostAdminControllerTest extends TestCase
+{
+    /**
+     * @test
+     * @return void
+     */
+    public function it_only_logged_user_can_make_changes_to_post()
+    {
+        $post = BlogPost::factory()->create();
+
+        $this
+            ->post(action([BlogPostAdminController::class, 'update'], $post->slug), [
+               'title' => 'test',
+               'author' => $post->author,
+               'body' => $post->body,
+               'date' => $post->date->format('Y-m-d')
+            ])
+        ->assertRedirect(route('login'));
+
+        $this->assertNotEquals('test', $post->refresh()->title);
+
+        $this->actingAs(User::factory()->create());
+
+        $this
+            ->post(action([BlogPostAdminController::class, 'update'], $post->slug), [
+                'title' => 'test',
+                'author' => $post->author,
+                'body' => $post->body,
+                'date' => $post->date->format('Y-m-d')
+            ]);
+
+        $this->assertEquals('test', $post->refresh()->title);
+    }
+}
